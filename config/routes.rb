00c3e63bcbf47f1aed630c1065e35048c723ffac
filipe paiva <filipe.p@soltroiavillas.com>
2017-02-27 17:53:12 +0000

    controllers and views for groups memberships

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'group_memberships/show'
+
+  get 'group_memberships/new'
+
+  get 'group_memberships/create'
+
+  get 'group_memberships/update'
+
+  get 'group_memberships/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
