08e1ea8d6abecb0f8d23e93a6f6ecf497daf517a
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-28 11:57:51 +0000

    generate random first question in home page

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,13 @@ class Question < ApplicationRecord
   belongs_to :conversation_history
   has_one :answer
 
-
+  def self.first_question
+    questions = [
+      "How are you today?",
+      "What are you up to?",
+      "How are you feeling?",
+      "What's on your mind?"
+    ]
+    questions.sample
+  end
 end
