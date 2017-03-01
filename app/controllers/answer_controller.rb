bc0259fb4cc56cccfc501bf9d3a2f00daaf6b590
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-03-01 13:02:06 +0000

    add pseudo code to answer and question controller

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,3 +1,10 @@
 class AnswersController < ApplicationController
 
+  def create
+    # needs to revceive a question
+    # create new answer with submituon from home page - store as content
+    # get sentiment from answr model .get_sentiment - store as sentimnt analysis
+    # redirect to questions controller
+  end
+
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,10 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def question_by_mood(sentiment_value)
+    # if sentiment_value > 1 => happy
+    # elsif sentiment_value <= 1 => sad
+    # find a quetsion in db based on type: sad or happy
+  end
+
 end
