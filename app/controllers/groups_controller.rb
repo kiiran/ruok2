9daec5c54d51c29e2a1c3c613aa85a08f1fa5b80
Pedro Rio <prio@teachme.site>
2017-03-01 14:02:04 +0000

    What? Created group controller, views and associated routes. Why? To make it work.

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,44 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  def index
+    @groups = Group.all
+  end
+
+  def show
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.member = current_user
+    @group.members.first.admin = true
+    if @group.save
+      redirect_to user_group(@group)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @group.update(group_params)
+  end
+
+  def destroy
+    @group.destroy
+  end
+
+private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+end
