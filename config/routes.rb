2a8956a94e07bdbdbcf60a1e81dcbc82153dcd7d
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-03-01 14:13:00 +0000

    create method in answr controller receives a string and a question object from homepage

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   end
   resources :conversation_histories, only: :new
   resources :template_questions, only: [:index, :show]
+  resources :answers, only: :create
 end
