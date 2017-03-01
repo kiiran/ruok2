b635bf4014f566fad517334c165a74eb405b9af1
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-03-01 11:52:17 +0000

    put x_mashape_key in application.yml(hidden)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,11 +3,10 @@ class Answer < ApplicationRecord
 
   def get_sentiment
     @text = self.content
-    @x_mashape_key = "RXl94q5a12mshmKgwfPamJO9J2TBp1dyeKhjsnRH8HxCkDVnuO"
     # These code snippets use an open-source library. http://unirest.io/ruby
     response = Unirest.post "https://japerk-text-processing.p.mashape.com/sentiment/",
     headers:{
-      "X-Mashape-Key" => @x_mashape_key,
+      "X-Mashape-Key" => ENV["x_mashape_key"],
       "Content-Type" => "application/x-www-form-urlencoded",
       "Accept" => "application/json"
     },
