ece67d7b5733d55191aa100da606fee5b156deb9
Ciaran Edwards <ciaran.edwards@live.com>
2017-03-02 11:58:06 +0000

    Fixing a bug with the background jobs (welcome email)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  after_save :welcome_email # Run on create & update
 
   # def create
   #   photo = JSON.parse(params['user']['photo'])
@@ -17,9 +16,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :photo, :password, :password_confirmation, :current_password)
-  end
-
-  def welcome_email
-    UserMailer.welcome(self.id).deliver_later
   end
 end
