244df90e814e8cfda5ca6d1778a3c020872cdad8
Ciaran Edwards <ciaran.edwards@live.com>
2017-03-06 10:43:58 +0000

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ GroupMembership.destroy_all
 Group.destroy_all
 User.destroy_all
 
-TemplateQuestion.create(id: 1, subject: "opening_question", content: "How are you today?")
-TemplateQuestion.create(id: 2, subject: "opening_question", content: "Tell me about your day")
-TemplateQuestion.create(id: 3, subject: "opening_question", content: "How are you feeling?")
-TemplateQuestion.create(id: 4, subject: "opening_question", content: "What's on your mind?")
+TemplateQuestion.create(subject: "opening_question", content: "How are you today?")
+TemplateQuestion.create(subject: "opening_question", content: "Tell me about your day")
+TemplateQuestion.create(subject: "opening_question", content: "How are you feeling?")
+TemplateQuestion.create(subject: "opening_question", content: "What's on your mind?")
 
 TemplateQuestion.create(subject: "neg", content: "Sounds like you're not having the best time. What could you do to make things better?")
 TemplateQuestion.create(subject: "neg", content: "Oh dear. How could you make things better?")
@@ -31,12 +31,12 @@ Group.create(name: "David the master")
 Group.create(name: "Filipe the master")
 Group.create(name: "Joao the master")
 
-Question.create(conversation_history_id: home_conversations.id, subject: "home", content: "hello?1", template_question_id: 1)
-Question.create(conversation_history_id: home_conversations.id, subject: "home", content: "hello?2", template_question_id: 2)
-Question.create(conversation_history_id: home_conversations.id, subject: "home", content: "hello?3", template_question_id: 3)
+q1 = Question.create(conversation_history_id: home_conversations.id, subject: "home", content: "hello?1", template_question_id: 1)
+q2 = Question.create(conversation_history_id: home_conversations.id, subject: "home", content: "hello?2", template_question_id: 2)
+q3 = Question.create(conversation_history_id: home_conversations.id, subject: "home", content: "hello?3", template_question_id: 3)
 
 
-# Answer.create(question_id: 1, pos: rand(1..100).to_f, neutral: rand(1..100).to_f, neg: rand(1..100).to_f, content: "hello 1")
-# Answer.create(question_id: 2, pos: rand(1..100).to_f, neutral: rand(1..100).to_f, neg: rand(1..100).to_f, content: "hello 2")
-# Answer.create(question_id: 3, pos: rand(1..100).to_f, neutral: rand(1..100).to_f, neg: rand(1..100).to_f, content: "hello 3")
+Answer.create(question_id: q1.id, pos: (rand(1..100).to_f / 100), neutral: rand(1..100).to_f / 100, neg: (rand(1..100).to_f / 100), content: "hello 1")
+Answer.create(question_id: q2.id, pos: (rand(1..100).to_f / 100), neutral: (rand(1..100).to_f / 100), neg: (rand(1..100).to_f / 100), content: "hello 2")
+Answer.create(question_id: q3.id, pos: (rand(1..100).to_f / 100), neutral: (rand(1..100).to_f / 100), neg: (rand(1..100).to_f / 100), content: "hello 3")
 
