52804fc6e4ae19b8207e547aec26b96097170d6f
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-03-08 13:50:11 +0000

    img placeholders in homepage

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -36,9 +36,9 @@ class GroupsController < ApplicationController
   end
 
   def send_questionnaire_to
-    @group.send_questionnaire_to
+    # @group.send_questionnaire_to
   end
-  
+
   def form
     @new_convo_history = ConversationHistory.new
     @new_convo_history.user = User.first
