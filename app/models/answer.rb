8b280b2219ee54040cdec867c8b02fbe4a926349
Pedro Rio <prio@teachme.site>
2017-03-09 18:29:42 +0000

    Solved the bug

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -17,6 +17,7 @@ class Answer < ApplicationRecord
   def get_sentiment
     @text = self.content
     # These code snippets use an open-source library. http://unirest.io/ruby
+    # TODO make syncronous request
     response = Unirest.post "https://japerk-text-processing.p.mashape.com/sentiment/",
     headers:{
       "X-Mashape-Key" => ENV["x_mashape_key"],
