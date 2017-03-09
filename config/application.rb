d985a07baa5d9e3c117eff9d989bc4c0966ed836
Ciaran Edwards <ciaran.edwards@live.com>
2017-03-09 11:58:02 +0000

    Made 404 and 500 error pages

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Ruok2
       generate.assets false
     end
     config.active_job.queue_adapter = :sidekiq
+    config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
